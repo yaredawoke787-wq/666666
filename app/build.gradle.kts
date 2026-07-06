a83906c40424d6e50244eb1daf6ccc4d23015577
yaredawoke787-wq <yaredawoke787@gmail.com>
2026-07-06 15:14:59 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,6 @@ android {
       signingConfig = signingConfigs.getByName("release")
     }
     debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
-    }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
